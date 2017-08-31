1b2a974cc347a34fb5c8b2a3920018c1315585bb
Marko <markthomasnoonan@gmail.com>
2017-08-31 13:43:21 -0400

    Update bus image
    Increase height of ETA display bar

--- mobility-eta.js
+++ mobility-eta.js
@@ -95,7 +95,7 @@ function showTrips() {
     '<div class="bookings-wrapper" style="display: inline-block;padding:20px;font-size: 14px;text-align: center;font-family: sans-serif;"><div class="client-info text-align-center"> Hi, <b>' +
     clientName + '</b>    </div>' + '<h1>Your Bookings</h1>';
 
-  bookings.forEach(function(booking, index) {
+  bookings.forEach(function(booking, i) {
     htmlBuffer +=
       '<div class="booking plan-a-trip-box" style="box-shadow: 0 0 3px 2px rgba(200,200,200,0.7); border-radius: 4px; margin: 10px; padding: 0 10px 10px 8px; padding-bottom: 6px; width: 310px; text-align: center; display:inline-block;"> <div class="date-and-id" id="date-and-id"> <span class="display-date"><h2 style="border:0; margin-bottom: 4px; margin-top: 12px"><b>' +
       booking.displayDate + '</b></h2></span><span><b>Booking ID</b>: ' + booking.bookingID +
@@ -106,7 +106,7 @@ function showTrips() {
       booking.displayReadyTime +
       '</span><br>&nbsp;▼</div> <div class="label center" style="width: auto;text-align: left;display: inline-block;transform: translateX(45px);padding: 0"><span class="tool-tip" style="background-color: #fff;color: #000;border-radius: 3px;display: inline-block;padding: 3px;margin-bottom: -2px;"><b>End Window</b><br>' +
       booking.displayEndWindow +
-      '</span><br>&nbsp;▼</div></div> <div class="outer" style="text-align: left; width: 240px; background-color: #111; border-radius: 10px; overflow: hidden;"> <div class="inner" style="height: 18px; background-color: #595; background: linear-gradient(to right, #595 0%, #ee5 50%, #f55 80%);"> <span class="eta label" style="position: relative;display: inline-block;box-sizing: border-box;height: 100%;padding-top: 5px;text-align: left;transform: translateY(-5px);" aria-hidden="true"><img src="assets/martabus.png" id="martabus' + index + '" style="height: 18px; display: inline; padding: 0 5px"/></span> </div> </div> <div class="eta-tooltip" style="width: auto;text-align: left;"> &nbsp;▲<br><span class="tool-tip" style="background-color: #fff;color: #000;border-radius: 3px;display: inline-block;padding: 3px;transform: translateY(-6px); line-height:1.2"><b>Your ETA</b><br>' + (booking.displayEta || "No ETA yet") + '</span></div></div>';
+      '</span><br>&nbsp;▼</div></div> <div class="outer" style="text-align: left; width: 240px; background-color: #111; border-radius: 10px; overflow: hidden;"> <div class="inner" style="height: 26px; background-color: #595; background: linear-gradient(to right, #595 0%, #ee5 50%, #f55 80%);"> <span class="eta label" style="position: relative;display: inline-block;box-sizing: border-box;height: 100%;padding-top: 5px;text-align: left;transform: translateY(-5px);" aria-hidden="true"><img src="assets/bus.png" id="martabus' + i + '" style="height: 26px; display: inline; padding: 0 5px 0 0"/></span> </div> </div> <div class="eta-tooltip" style="width: auto;text-align: left;"> &nbsp;▲<br><span class="tool-tip" style="background-color: #fff;color: #000;border-radius: 3px;display: inline-block;padding: 3px;transform: translateY(-6px); line-height:1.2"><b>Your ETA</b><br>' + (booking.displayEta || "No ETA yet") + '</span></div></div>';
 
     if (booking.status === "Scheduled") {
       htmlBuffer += '<span class="eta-wrapper" style="padding-top: 2px; display: inline-block; font-size: 14px; line-height: 1.3">Bus expected at <b>' + (booking.displayEta || "No ETA yet") + '</b>,<br>' + (booking.delayInMinutesDescription || "") + '</span>';
@@ -150,7 +150,7 @@ function setMarker() {
 
   document.querySelector("#martabus0").style.borderLeft = markerDistance + borderStyle;
   document.querySelector("#martabus0").style.borderRight = remainingDistance + borderStyle;
-  document.querySelector(".eta-tooltip").style.marginLeft = markerDistance;
+  document.querySelector(".eta-tooltip").style.marginLeft = (markerNumber - 8) + "px";
 
   document.querySelector(".progress-wrapper").style.display = "inline-block";
   //the last line is just a quick hack to only show this on the first trip.
